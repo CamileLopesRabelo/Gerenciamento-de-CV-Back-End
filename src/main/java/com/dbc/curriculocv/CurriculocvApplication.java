a3df7c748f7c270ebded9506cce3e114c2844ee9
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-12-09 15:11:04 -0300

    feat: initializer spring

---
+++ src/main/java/com/dbc/curriculocv/CurriculocvApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.curriculocv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculocvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculocvApplication.class, args);
+	}
+
+}
