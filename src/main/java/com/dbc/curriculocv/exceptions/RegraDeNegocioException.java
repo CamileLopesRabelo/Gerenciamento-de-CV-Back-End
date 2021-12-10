31d8ef3468cf6f94cffc866331066eb924ff2cd8
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-12-10 13:30:08 -0300

    feat: add esceptions

---
+++ src/main/java/com/dbc/curriculocv/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.dbc.curriculocv.exceptions;
+
+public class RegraDeNegocioException extends Exception {
+    public RegraDeNegocioException(String mensagem){
+        super(mensagem);
+    }
+}
