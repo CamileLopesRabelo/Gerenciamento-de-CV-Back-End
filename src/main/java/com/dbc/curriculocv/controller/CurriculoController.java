0232339180f408124b71e84d97856ac7fe7e4b71
Brunno Zonatto <37561155+brzonatto@users.noreply.github.com>
2021-12-14 17:48:25 -0300

    fix: ajuste controller e add cripname  no curriculo

--- src/main/java/com/dbc/curriculocv/controller/CurriculoController.java
+++ src/main/java/com/dbc/curriculocv/controller/CurriculoController.java
@@ -1,53 +1,29 @@
 package com.dbc.curriculocv.controller;
 
 import com.dbc.curriculocv.dto.CurriculoDTO;
-import com.dbc.curriculocv.entity.Curriculo;
 import com.dbc.curriculocv.exceptions.RegraDeNegocioException;
 import com.dbc.curriculocv.service.CurriculoService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@RequiredArgsConstructor
 @RestController
+@RequestMapping("/curriculo")
+@Validated
+@RequiredArgsConstructor
 public class CurriculoController {
-    private final ObjectMapper objectMapper;
-    private static final Logger logger = LoggerFactory.getLogger(CurriculoController.class);
+    private final CurriculoService curriculoService;
 
-    @Autowired
-    private CurriculoService curriculoService;
-
-    @PostMapping("/uploadFile")
-    public CurriculoDTO uploadFile(@RequestPart("file") MultipartFile file, Integer idCandidato) throws RegraDeNegocioException {
-        Curriculo curriculo = curriculoService.storeFile(file,idCandidato);
-
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(curriculo.getIdCurriculo().toString())
-                .toUriString();
-
-        return objectMapper.convertValue(curriculo,CurriculoDTO.class);
+    @PostMapping("/upload-curriculo")
+    public CurriculoDTO uploadCurriculo(@RequestPart("file") MultipartFile file, @RequestParam("idCandidato") Integer idCandidato) throws RegraDeNegocioException {
+        return curriculoService.uploadCurriculo(file, idCandidato);
     }
 
-    @GetMapping("/downloadFile/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable Integer fileId) {
-        // Load file from database
-        Curriculo curriculo = curriculoService.getFile(fileId);
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(curriculo.getFileType()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + curriculo.getFileName() + "\"")
-                .body(new ByteArrayResource(curriculo.getData()));
+    @GetMapping("/download-curriculo/{idCandidato}")
+    public ResponseEntity<Resource> downloadCurriculo(@PathVariable Integer idCandidato) {
+        return curriculoService.doanloadCurriculo(idCandidato);
     }
-
 }
