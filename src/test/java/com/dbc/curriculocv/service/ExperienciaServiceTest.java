eabc799d9901d604579477a0f55ad391ba1e4f12
camile.oliveira <camilelopes.oliveira@gmail.com>
2021-12-18 12:05:39 -0300

    ExperienciaServiceTest concluido

---
+++ src/test/java/com/dbc/curriculocv/service/ExperienciaServiceTest.java
@@ -0,0 +1,115 @@
+package com.dbc.curriculocv.service;
+
+import com.dbc.curriculocv.dto.ExperienciasCreateDTO;
+import com.dbc.curriculocv.dto.ExperienciasDTO;
+import com.dbc.curriculocv.entity.Candidato;
+import com.dbc.curriculocv.entity.Experiencias;
+import com.dbc.curriculocv.exceptions.RegraDeNegocioException;
+import com.dbc.curriculocv.repository.CandidatoRepository;
+import com.dbc.curriculocv.repository.ExperienciasRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@DisplayName("ExperienciaServiceTest")
+public class ExperienciaServiceTest {
+
+    @InjectMocks
+    private ExperienciasService experienciasService;
+
+    @Mock
+    private ExperienciasRepository experienciasRepository;
+
+    @Mock
+    private CandidatoRepository candidatoRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void beforeEach() {
+        ReflectionTestUtils.setField(experienciasService, "objectMapper",objectMapper);
+    }
+
+    @Test
+    @DisplayName("deve listar as experiencias")
+    public void listarExperiencias() {
+        List<Experiencias> experiencias = new ArrayList<>();
+
+        when(experienciasRepository.findAll()).thenReturn(experiencias);
+
+        List<ExperienciasDTO> list = experienciasService.list();
+
+        assertNotNull(list);
+    }
+
+    @Test
+    @DisplayName("deve criar uma experiencia")
+    public void criarExperiencia() throws RegraDeNegocioException {
+        Candidato candidato = new Candidato();
+        candidato.setIdCandidato(1);
+        Experiencias experiencias = new Experiencias();
+        experiencias.setNomeEmpresa("DBC Company");
+        ExperienciasCreateDTO experienciasCreateDTO = new ExperienciasCreateDTO();
+
+        when(candidatoRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.of(candidato));
+        when(experienciasRepository.save(ArgumentMatchers.any())).thenReturn(experiencias);
+
+        ExperienciasDTO experienciasDTO = experienciasService.create(candidato.getIdCandidato(),experienciasCreateDTO);
+
+        assertNotNull(experienciasDTO);
+        assertEquals("DBC Company",experienciasDTO.getNomeEmpresa());
+    }
+
+    @Test
+    @DisplayName("deve alterar os dados da experiencia")
+    public void alterarExperiencias() throws RegraDeNegocioException {
+        Experiencias experiencias =  new Experiencias();
+        experiencias.setIdExperiencia(1);
+        Experiencias experienciasSave = new Experiencias();
+        experienciasSave.setNomeEmpresa("DBC Company");
+
+        ExperienciasCreateDTO experienciasCreateDTO = new ExperienciasCreateDTO();
+        experiencias.setNomeEmpresa("DBC Company");
+
+        when(experienciasRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.of(experiencias));
+        when(experienciasRepository.save(ArgumentMatchers.any())).thenReturn(experienciasSave);
+
+        ExperienciasDTO update = experienciasService.update(experiencias.getIdExperiencia(), experienciasCreateDTO);
+
+        assertNotNull(update);
+        assertEquals("DBC Company",update.getNomeEmpresa());
+
+    }
+
+    @Test
+    @DisplayName("deve deletar experiencias")
+    public void deletarExperiencias() throws RegraDeNegocioException {
+        Experiencias experiencias = new Experiencias();
+        experiencias.setIdExperiencia(1);
+
+        when(experienciasRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.of(experiencias));
+        doNothing().when(experienciasRepository).delete(experiencias);
+
+        experienciasService.delete(experiencias.getIdExperiencia());
+
+        verify(experienciasRepository,times(1)).delete(ArgumentMatchers.any());
+        verify(experienciasRepository,times(1)).findById(ArgumentMatchers.any());
+    }
+
+}
---
+++ src/test/java/com/dbc/curriculocv/service/VagasCompleoServiceTest.java
@@ -0,0 +1,83 @@
+package com.dbc.curriculocv.service;
+
+import com.dbc.curriculocv.client.VagasCompleoClient;
+import com.dbc.curriculocv.dto.VagasCompleoDTO;
+import com.dbc.curriculocv.dto.VagasDTO;
+import com.dbc.curriculocv.dto.VagasFiltradasDTO;
+import com.dbc.curriculocv.entity.Vaga;
+import com.dbc.curriculocv.repository.VagaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+@DisplayName("VagasCompleoServiceTest")
+public class VagasCompleoServiceTest {
+
+    @InjectMocks
+    private VagasCompleoService vagasCompleoService;
+
+    @Mock
+    private VagasCompleoClient vagasCompleoClient;
+
+    @Mock
+    private VagaRepository vagaRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void beforeEach() {
+        ReflectionTestUtils.setField(vagasCompleoService, "objectMapper",objectMapper);
+    }
+
+    @Test
+    @DisplayName("deve listar as vagas da api do compleo")
+    public void listarVagasCompleo() {
+
+        List<Vaga> vagas = new ArrayList<>();
+        List<VagasCompleoDTO> listaDeVagasCompleo = new ArrayList<>();
+        VagasDTO vagasDTO = new VagasDTO();
+        vagasDTO.setVagaGeralList(listaDeVagasCompleo);
+        vagasDTO.setPaginas(10);
+        vagasDTO.setPagina(1);
+        vagasDTO.setQuantidade(10);
+
+
+        when(vagasCompleoClient.listar(ArgumentMatchers.any(),ArgumentMatchers.any(),ArgumentMatchers.any())).thenReturn(vagasDTO);
+        when(vagaRepository.findAll()).thenReturn(vagas);
+        VagasFiltradasDTO list = vagasCompleoService.list();
+
+        assertNotNull(list);
+        assertEquals(vagasDTO.getVagaGeralList(),list.getVagaGeralList());
+
+    }
+
+    @Test
+    @DisplayName("deve filtrar as vagas que estão com status em aberto no compleo")
+    public void filtrarVagas() {
+        VagasDTO vagaDTO = new VagasDTO();
+        List<Vaga> vagas = new ArrayList<>();
+
+        when(vagaRepository.findAll()).thenReturn(vagas);
+
+        List<VagasCompleoDTO> listaFiltrada = vagasCompleoService.filtrarVagas(vagaDTO);
+
+        assertNotNull(listaFiltrada);
+    }
+
+
+}
