c3022edc1e31d37fd2da600234670a6ae2a0b161
camile.oliveira <camilelopes.oliveira@gmail.com>
2021-12-19 12:25:45 -0300

    testes concluidos

--- src/test/java/com/dbc/curriculocv/service/UsuarioServiceTest.java
+++ src/test/java/com/dbc/curriculocv/service/UsuarioServiceTest.java
@@ -1,50 +1,80 @@
 package com.dbc.curriculocv.service;
 
+import com.dbc.curriculocv.dto.*;
+import com.dbc.curriculocv.entity.Usuario;
+import com.dbc.curriculocv.exceptions.RegraDeNegocioException;
 import com.dbc.curriculocv.repository.UsuarioRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class UsuarioServiceTest {
-    @Mock
-    private UsuarioRepository usuarioRepository;
-    @Mock
-    private ObjectMapper objectMapper;
+@RunWith(MockitoJUnitRunner.class)
+@DisplayName("Usuário Service Testes")
+public class UsuarioServiceTest {
 
     @InjectMocks
     private UsuarioService usuarioService;
 
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this);
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void beforeEach() {
+        ReflectionTestUtils.setField(usuarioService, "objectMapper", objectMapper);
     }
 
     @Test
-    void deveRetornarUmEmailCadastrado() {
-        String email = "email@email.com.br";
-        usuarioService.findByEmail(email);
-        verify(usuarioRepository, times(1)).findByEmail(email);
+    @DisplayName("Deve criar um novo usuário")
+    public void deveCriarUmNovoUsuario() throws RegraDeNegocioException {
+        BCryptPasswordEncoder crypt = new BCryptPasswordEncoder();
+        UsuarioCreateDTO usuarioCreateDTO = new UsuarioCreateDTO();
+        usuarioCreateDTO.setEmail("teste@teste.com.br");
+        usuarioCreateDTO.setSenha("teste");
+        Usuario usuarioSave = new Usuario();
+        usuarioSave.setIdUsuario(1);
+        usuarioSave.setEmail("teste@teste.com.br");
+        usuarioSave.setSenha(crypt.encode(usuarioCreateDTO.getSenha()));
+
+
+        when(usuarioRepository.existsByEmail(usuarioCreateDTO.getEmail())).thenReturn(false);
+        when(usuarioRepository.save(any())).thenReturn(usuarioSave);
+
+        UsuarioDTO usuarioDTO = usuarioService.create(usuarioCreateDTO);
+        assertNotNull(usuarioDTO);
+        assertEquals(1, usuarioDTO.getIdUsuario());
+        assertEquals("teste@teste.com.br", usuarioDTO.getEmail());
     }
 
-//    @Test
-//    void deveCriarUmNovoUsuario() throws RegraDeNegocioException {
-//        Usuario usuario = new Usuario();
-//        UsuarioCreateDTO usuarioCreateDTO = new UsuarioCreateDTO("sdas", "sadsa@gmail.com", "das*B545da");
-//
-////        when(objectMapper.convertValue(usuarioCreateDTO, Usuario.class)).thenReturn(usuario);
-////        doReturn(usuarioRepository.save(usuario)).when(any());
-//
-//        usuarioService.create(usuarioCreateDTO);
-//
-//        verify(usuarioRepository, times(1)).save(usuario);
-//    }
+    @Test(expected = RegraDeNegocioException.class)
+    @DisplayName("Deve cair na exceção ao criar um novo usuário")
+    public void deveCriarUmNovoUsuarioSemSucesso() throws RegraDeNegocioException {
+        BCryptPasswordEncoder crypt = new BCryptPasswordEncoder();
+        UsuarioCreateDTO usuarioCreateDTO = new UsuarioCreateDTO();
+        usuarioCreateDTO.setEmail("teste@teste.com.br");
+        usuarioCreateDTO.setSenha("teste");
+        Usuario usuarioSave = new Usuario();
+        usuarioSave.setIdUsuario(1);
+        usuarioSave.setEmail("teste@teste.com.br");
+        usuarioSave.setSenha(crypt.encode(usuarioCreateDTO.getSenha()));
 
 
+        when(usuarioRepository.existsByEmail(usuarioCreateDTO.getEmail())).thenReturn(true);
+
+        UsuarioDTO usuarioDTO = usuarioService.create(usuarioCreateDTO);
+        assertNotNull(usuarioDTO);
+        assertEquals(1, usuarioDTO.getIdUsuario());
+        assertEquals("teste@teste.com.br", usuarioDTO.getEmail());
+    }
 }
 
